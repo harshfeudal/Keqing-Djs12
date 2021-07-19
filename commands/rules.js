b73bec2b405a8561e42a49d51a99cdf985da9091
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2021-07-19 22:33:11 +0700

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -4,7 +4,7 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'rules',
     description: "automate rules bot",
-    async execute (bot, message, args) {
+    async execute(message, args) {
         
         const BotRules = new Discord.MessageEmbed()
         .setColor(0x85FFF2)
