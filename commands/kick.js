67acadd444f5496eb5821a71bc31293d610b15a7
harshfeudal <87577447+harshfeudal@users.noreply.github.com>
2021-07-19 23:06:35 +0700

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -2,13 +2,17 @@ module.exports = {
     name: 'kick',
     description: "Keqing will kick member the has mentioned",
     execute(message, args){
-        const member = message.mentions.users.first();
-        if(member){
-            const memberTarget = message.guild.members.cache.get(member.id);
-            memberTarget.kick();
-            message.channel.send("Keqing has kicked that user :( Sorry ;-;")
-        } else {
-            message.channel.send('Keqing cannot kick that user.');
+        if (message.member.roles.cache.has('854767607193403463')){
+            const member = message.mentions.users.first();
+            if(member){
+                const memberTarget = message.guild.members.cache.get(member.id);
+                memberTarget.kick();
+                message.channel.send("Keqing has kicked that user :( Sorry ;-;")
+            } else {
+                message.channel.send('Keqing cannot kick that user.');
+                }
+            } else {
+                message.channel.send('You cannot tell me to do that :(');
         }
     }
 }
