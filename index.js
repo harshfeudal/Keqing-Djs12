720f4fb9f1c5d6eef2db3c597492ab83492638eb
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-27 18:43:49 +0700

    [372005]: Delete package-lock.json and lint index.js

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
-const Discord = require('discord.js');
+const Discord = require('discord.js')
 
 require('dotenv').config()
 
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTION"]});
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] })
 
-const prefix = 'k?';
+const prefix = 'k?'
 
-const fs = require('fs');
+const fs = require('fs')
 
-client.commands = new Discord.Collection();
+client.commands = new Discord.Collection()
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
-for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'))
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
 
-    client.commands.set(command.name, command);
+  client.commands.set(command.name, command)
 }
 
-
 client.on('ready', () => {
-    client.user.setPresence({ activity: { name: 'Watching my husband Harshfeudal fixing me. Call me by using [k?] <3' }, status: 'dnd' });
-	console.log(`${client.user.tag} is online :)`);
-});
+  client.user.setPresence({ activity: { name: 'Watching my husband Harshfeudal fixing me. Call me by using [k?] <3' }, status: 'dnd' })
+  console.log(`${client.user.tag} is online :)`)
+})
 
-client.on('message', message =>{
-    if(!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return;
-    message.content.replace(prefix, '').split(' ');
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
+client.on('message', message => {
+  if (!message.content.toLowerCase().startsWith(prefix) || message.author.bot) return
+  message.content.replace(prefix, '').split(' ')
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const commandName = args.shift().toLowerCase()
 
-    const command = client.commands.get(commandName)
-    if (!command) return message.channel.send('I dont know what you want to tell me something :(');
-    command.execute(message, args);
-});
+  const command = client.commands.get(commandName)
+  if (!command) return message.channel.send('I dont know what you want to tell me something :(')
+  command.execute(message, args)
+})
 
-const http = require('http');
+const http = require('http')
 const server = http.createServer((req, res) => {
-  res.writeHead(200);
-  res.end('ok');
-});
-server.listen(3000);
+  res.writeHead(200)
+  res.end('ok')
+})
+server.listen(3000)
 
-client.login(process.env.token);
+client.login(process.env.token)
