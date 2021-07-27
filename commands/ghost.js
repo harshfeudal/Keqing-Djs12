e5a36a1d4074b3aa83dcccd0a0e150e48173ffef
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-27 10:54:09 +0000

    [372005]

---
+++ commands/ghost.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: 'ghost',
+  async execute (message, args) {
+    message.channel.send('Warning! Currently only support 1 mention, 372005 is too lazy atm, will change this later')
+    if(!message.mentions.members.size) return message.channel.send('You need to ping the person who is now dead / a ghost')
+    message.mentions.members.first().roles.add(await message.guild.roles.fetch('869252442032840714'))
+      .then(() => message.channel.send(`Successfully make ${message.mentions.members.first().displayName} dead`))
+      .catch(error => {
+        console.log(error)
+        message.channel.send('An error occurred. Please tell 372005 about this. Error:' + error)
+      })
+  }
+}
