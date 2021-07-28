25d5770fef1c721487bef018eae259de071baf12
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-28 19:29:00 +0700

    hotfix2

--- commands/main/mute.js
+++ commands/main/mute.js
@@ -1,28 +1,29 @@
-const ms = require('ms')
-const { member_role_id, mute_role_id } = require('../../config.json')
+// const ms = require('ms')
+// const { member_role_id, mute_role_id } = require('../../config.json')
 module.exports = {
 	description: 'Keqing will mute member the has mentioned',
 	run (bot, message, args) {
-		if (!message.member.hasPermission('')) return message.channel.send('You cannot tell me to do that :(')
-		if (args[1] && !ms(args[1])) return message.channel.send('I dont know what you mean :( (Invalid duration)')
+		message.channel.send('<@622450109317251088> Please tell 372005 what permission does this command need')
+		// if (!message.member.hasPermission('')) return message.channel.send('You cannot tell me to do that :(')
+		// if (args[1] && !ms(args[1])) return message.channel.send('I dont know what you mean :( (Invalid duration)')
 
-		const member = message.mentions.members.first()
-		if (!member) return message.channel.send('You must mention someone to mute')
-		try {
-			const member_role = message.guild.roles.cache.get(member_role_id)
-			const mute_role = message.guild.roles.cache.get(mute_role_id)
-			member.roles.remove(member_role)
-			member.roles.add(mute_role)
-			message.channel.send(`Keqing has muted <@${member.user.id}> ${args[1] ? `for ${ms(ms(args[1]), { long: true })} ` : ''}:( Sorry ;-;`)
-			if (args[1]) {
-				setTimeout(() => {
-					member.roles.remove(mute_role)
-					member.roles.add(member_role)
-					message.channel.send(`Keqing has unmuted <@${member.user.id}> :)`)
-				}, ms(args[1]))
-			}
-		} catch {
-			message.channel.send('Keqing cannot mute that user.')
-		}
+		// const member = message.mentions.members.first()
+		// if (!member) return message.channel.send('You must mention someone to mute')
+		// try {
+		// 	const member_role = message.guild.roles.cache.get(member_role_id)
+		// 	const mute_role = message.guild.roles.cache.get(mute_role_id)
+		// 	member.roles.remove(member_role)
+		// 	member.roles.add(mute_role)
+		// 	message.channel.send(`Keqing has muted <@${member.user.id}> ${args[1] ? `for ${ms(ms(args[1]), { long: true })} ` : ''}:( Sorry ;-;`)
+		// 	if (args[1]) {
+		// 		setTimeout(() => {
+		// 			member.roles.remove(mute_role)
+		// 			member.roles.add(member_role)
+		// 			message.channel.send(`Keqing has unmuted <@${member.user.id}> :)`)
+		// 		}, ms(args[1]))
+		// 	}
+		// } catch {
+		// 	message.channel.send('Keqing cannot mute that user.')
+		// }
 	}
 }
--- commands/main/unmute.js
+++ commands/main/unmute.js
@@ -1,19 +1,20 @@
-const { member_role_id, mute_role_id } = require('../../config.json')
+// const { member_role_id, mute_role_id } = require('../../config.json')
 module.exports = {
 	description: 'Keqing will unmute member the has mentioned',
 	run (bot, message, args) {
-		if (!message.member.hasPermission('')) return message.channel.send('You cannot tell me to do that :(')
+		message.channel.send('<@622450109317251088> Please tell 372005 what permission does this command need')
+		// if (!message.member.hasPermission('')) return message.channel.send('You cannot tell me to do that :(')
 
-		const member = message.mentions.members.first()
-		if (!member) return message.channel.send('You must mention someone to unmute')
-		try {
-			const member_role = message.guild.roles.cache.get(member_role_id)
-			const mute_role = message.guild.roles.cache.get(mute_role_id)
-			member.roles.add(member_role)
-			member.roles.remove(mute_role)
-			message.channel.send(`Keqing has unmuted <@${member.user.id}>`)
-		} catch {
-			message.channel.send('Keqing cannot unmute that user.')
-		}
+		// const member = message.mentions.members.first()
+		// if (!member) return message.channel.send('You must mention someone to unmute')
+		// try {
+		// 	const member_role = message.guild.roles.cache.get(member_role_id)
+		// 	const mute_role = message.guild.roles.cache.get(mute_role_id)
+		// 	member.roles.add(member_role)
+		// 	member.roles.remove(mute_role)
+		// 	message.channel.send(`Keqing has unmuted <@${member.user.id}>`)
+		// } catch {
+		// 	message.channel.send('Keqing cannot unmute that user.')
+		// }
 	}
 }
