4a09f24b7d50bdd5869a452124bf46723942d8de
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-28 21:49:21 +0700

    hotfix9

--- commands/main/unban.js
+++ commands/main/unban.js
@@ -3,7 +3,7 @@ module.exports = {
 		if (!message.member.hasPermission('BAN_MEMBERS')) return message.channel.send('You cannot tell me to do that')
 		let bans = await message.guild.fetchBans()
 		bans = bans.map(ban => `To unban ${ban.user.tag} send k?unban ${ban.user.id}`).join('\n')
-		if (!args.length) message.channel.send(bans || 'No bans found.')
+		if (!args.length) return message.channel.send(bans || 'No bans found.')
 		if (args[0] && !bot.users.cache.get(args[0])) return message.channel.send('I dont know what you mean :( (Invalid User ID, send k?unban for a ban list)')
 		try {
 			message.guild.members.unban(bot.users.cache.get(args[0]))
