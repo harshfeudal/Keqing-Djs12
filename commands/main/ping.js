9d7903b3a60e18287fae1c05da7b6b8f90b7dbc7
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-29 18:20:27 +0700

    hotfix14

--- commands/main/ping.js
+++ commands/main/ping.js
@@ -1,6 +1,6 @@
 module.exports = {
 	description: 'this is a ping command!',
-	run (bot, message, args) {
-		message.channel.send('pong!')
+	async run (bot, message, args) {
+		message.channel.send('Pinging...').then(msg => msg.edit(`Pong! The message round-trip took ${(msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)}ms. The heartbeat ping is ${Math.round(bot.ws.ping)}ms.`))
 	}
 }
