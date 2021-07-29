f913d242f16f588e84b2f6f3aa00efbc6832edc2
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-29 18:41:12 +0700

    [372005]: 1.0.0.0

---
+++ commands/372005/help.js
@@ -0,0 +1,29 @@
+const { prefix } = require('../../config.json')
+
+module.exports = {
+	async run (bot, message, args) {
+		const data = []
+		const { commands } = bot
+
+		if (!args.length) {
+			data.push('Here\'s a list of all my commands!')
+			data.push(commands.filter(command => !command.owner_only).map(command => command.name).join(', '))
+			data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`)
+
+			message.channel.send(data.join('\n'), { split: true })
+
+			return
+		}
+		const name = args[0].toLowerCase()
+		const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name))
+
+		if (!command) return message.channel.send('Unknown command!')
+		if (command.owner_only) return message.channel.send('Unknown command! ||or is it?||')
+
+		data.push(`**Name:** ${command.name}`)
+
+		if (command.description) data.push(`**Description:** ${command.description}`)
+
+		message.channel.send(data.join('\n'), { split: true })
+	}
+}
---
+++ commands/372005/reload.js
@@ -0,0 +1,22 @@
+const { owners } = require('../../config.json')
+module.exports = {
+	async run (bot, message, args) {
+		if (!owners.includes(message.author.id)) return message.channel.send('The reload command can only be used by the bot owners')
+		if (!args.length) return message.channel.send('You didn\'t provide any arguments')
+		const command_name = args[0].toLowerCase()
+		const command = bot.commands.get(command_name) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command_name))
+
+		if (!command) return message.channel.send('Unknown command!')
+
+		delete require.cache[require.resolve(`../${command.group}/${command.name}.js`)]
+
+		const new_command = require(`../${command.group}/${command.name}.js`)
+
+		new_command.group = command.group
+		new_command.name = command.name
+
+		bot.commands.set(new_command.name, new_command)
+
+		message.channel.send('Reloaded command!')
+	}
+}
