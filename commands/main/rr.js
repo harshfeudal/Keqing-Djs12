afe8bf5b3f351a9effbfc3196f891777777f85fa
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-29 19:26:02 +0700

    [Version]: 1.0.0.4

--- commands/main/rr.js
+++ commands/main/rr.js
@@ -1,9 +1,10 @@
 const Discord = require('discord.js')
-const { russia_emoji_name, spanish_emoji_name, english_emoji_name, vietnam_emoji_name } = require('../../config.json')
+const { owners, russia_emoji_name, spanish_emoji_name, english_emoji_name, vietnam_emoji_name } = require('../../config.json')
 
 module.exports = {
 	description: 'Keqing will add a role for who has reacted',
 	async run (bot, message, args) {
+		if (!owners.includes(message.author.id)) return message.channel.send('The reload command can only be used by the bot owners')
 		const embed = new Discord.MessageEmbed()
 			.setColor('#85FFF2')
 			.setTitle('Welcome to Geometry Dash hall!')
