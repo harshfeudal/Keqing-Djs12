c0ba07ac59735d88032026a1a91fa12509114e70
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-29 10:46:15 +0700

    hotfix1

--- commands/main/roll.js
+++ commands/main/roll.js
@@ -2,6 +2,6 @@ module.exports = {
 	description: 'this is a lmao command!',
 	run (bot, message, args) {
 		const reply = ['nope', 'yes', 'nevermind', 0,1,2,3,4,5,6]
-		message.channel.send(`${Math.floor(Math.random() * reply.length)} :)`)
+		message.channel.send(`${reply[Math.floor(Math.random() * reply.length)]} :)`)
 	}
 }
