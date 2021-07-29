41fefd60b7faa6ff36082c1f733b6905b6acc39d
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-29 18:57:04 +0700

    [Version]: 1.0.0.2

--- commands/main/verify.js
+++ commands/main/verify.js
@@ -1,8 +1,8 @@
-const { verify_role_id } = require('../../config.json')
+const { member_role_id } = require('../../config.json')
 module.exports = {
 	description: 'verify the participant',
 	run (bot, message, args) {
-		if (message.member.roles.cache.has(verify_role_id)) {
+		if (message.member.roles.cache.has(member_role_id)) {
 			message.channel.send('You are verified already.').then(msg => {
 				setTimeout(() => {
 					message.delete()
@@ -10,8 +10,8 @@ module.exports = {
 				}, 3000)
 			})
 		} else {
-			message.member.roles.add(verify_role_id)
-			message.member.roles.add(verify_role_id)
+			message.member.roles.add(member_role_id)
+			message.member.roles.add(member_role_id)
 			message.channel.send('You are now verified :)').then(msg => {
 				setTimeout(() => {
 					message.delete()
