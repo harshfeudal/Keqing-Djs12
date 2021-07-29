dc473d0fed055243b18c95ac3e28f314abe952fc
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-29 18:03:45 +0700

    hotfix13

--- commands/main/verify.js
+++ commands/main/verify.js
@@ -1,12 +1,12 @@
-const { member_role_id } = require('../../config.json')
+const { verify_role_id } = require('../../config.json')
 module.exports = {
 	description: 'verify the participant',
 	run (bot, message, args) {
-		if (message.member.roles.cache.has(member_role_id)) {
+		if (message.member.roles.cache.has(verify_role_id)) {
 			message.channel.send('You are verified already.')
 		} else {
-			message.member.roles.add(member_role_id)
-			message.member.roles.add(member_role_id)
+			message.member.roles.add(verify_role_id)
+			message.member.roles.add(verify_role_id)
 			message.channel.send('You are now verified :)')
 		}
 	}
