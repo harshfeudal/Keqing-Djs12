6e2601dd95230f952b196ed7759e9d5c698974fd
theo372005 <87189679+theo372005@users.noreply.github.com>
2021-07-30 11:11:43 +0700

    [Version]: 1.0.0.10

--- dashboard/index.js
+++ dashboard/index.js
@@ -5,11 +5,14 @@ const app = express()
 app.set('views', require('path').join(__dirname, '/views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => res.render('index'))
-
-app.get('/', (req, res) => res.send('<h1>Hi!</h1>'))
+app.get('/', (req, res) => {
+	res.status(200)
+	res.render('index')
+	res.send('<h1>Hi!</h1>')
+	res.end()
+})
 
 const port = process.env.PORT || 3000
-app.listen(port, () => console.log(`Server is live on port ${port}`))
+app.listen(port, () => console.log(`[Website] Server is live on port ${port}`))
 
 // use this by command prompt, cd to dashboard file and type: node server.js
